c7b0fcf5f1189c13ec6bd70e66823f3b4cdcfe5c
ehtishamhassan308 <ehtisham2285@gmail.com>
2025-07-31 18:39:53 +0500

    Initial commit - fresh start 8

--- config/api.ts
+++ config/api.ts
@@ -1,3 +1,3 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://web-production-813d.up.railway.app/api/v1";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://web-production-813d.up.railway.app";
 
 export default API_URL;
